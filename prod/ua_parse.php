2ed7f7185ed050cd3e88994f0c0314f4ddd852f2
dpp <dpp@dpp.su>
2016-01-03 22:21:16 +0200

    some comments + optional parameter to get only ua/os/device

--- prod/ua_parse.php
+++ prod/ua_parse.php
@@ -5,7 +5,7 @@
 
 $regexes = require __DIR__ . "/ua_regexes.php";
 
-function ua_parse($ua)
+function ua_parse($ua, $keys = ['ua', 'os', 'device'])
 {
     global $regexes;
     if (empty($ua)) {
@@ -13,9 +13,10 @@ function ua_parse($ua)
     }
 
     $result = [];
-    foreach (['ua', 'os', 'device'] as $key) {
+    foreach ($keys as $key) {
         foreach ($regexes[$key] as $regex => $replacement) {
             if (preg_match($regex, $ua, $match)) {
+                // can't use preg_replace here because regex is not matching whole string and result will have random trash except captured portions
                 array_shift($match);
                 $result[$key] = trim(str_replace(['$1', '$2', '$3', '$4'], $match, $replacement), ',');
                 continue 2;
