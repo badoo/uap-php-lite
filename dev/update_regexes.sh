5f0fe14b86d200fea0cf2e5016d2507a2fd2b6f7
dpp <dpp@dpp.su>
2016-07-12 12:11:12 +0300

    fix update script

--- dev/update_regexes.sh
+++ dev/update_regexes.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-#if [ "`dirname $0`" = "`pwd`" ]
-#then
-#	cd ..
-#fi
+set -ex
 
-git pull --quiet
-git submodule --quiet foreach git pull --quiet
-php ./dev/generate_ua.php ./prod/ua_regexes.php
-git commit --quiet -a -m "Update regexes" && git push --quiet origin master
+cd "$(git rev-parse --show-toplevel)"
+git pull
+git submodule foreach git pull
+cd dev
+php generate_ua.php ../prod/ua_regexes.php
+cd ..
+git commit -m "Update regexes" dev/spyc dev/uap-core prod/ua_regexes.php
+#git push origin master
 
